1177e8b858eb1b71634973c7dd23297b5a05cf1a
Ryan Day <soldair@gmail.com>
2010-12-21 01:31:21 -0800

    wooo first working api method! QRCode.toDataURL

---
+++ qrcode.js
@@ -0,0 +1,55 @@
+
+var QRCodeDraw = require(__dirname+'/lib/qrcode-draw')
+	, Canvas = require('canvas')
+	, fs = require('fs');
+
+exports.QRCodeDraw = QRCodeDraw;
+exports.version = '0.0.0';
+
+// returns Canvas Object with qr code drawn on it
+var draw = exports.draw = function(text,cb){
+	//NOTE the width and height are determined from within the qr code lib and are not configurable from the outside yet
+	QRCodeDraw.draw(new Canvas(200,200),text,function(error,canvas){
+		cb(error,canvas)
+	});
+}
+
+//returns data uri for drawn qrcode png
+var dataURL = exports.toDataURL = function(text,cb){
+	draw(text,function(error,canvas){
+		if(error) {
+			cb(error,'');
+		} else {
+			canvas.toDataURL(cb);
+		}
+	});
+}
+
+
+//returns bytes written to file 
+exports.save = function(path,text,cb){
+	draw(text,function(error,canvas){
+
+		var fd,buf,fdAndBuff = function(){
+			fs.write(fd, buff, 0, buf.length, 0, function(err,written){
+				fs.close(fd);
+				cb(err,written);
+			});
+		};
+
+		//run async calls at the same time ish so they can take advantage of the others idle time
+		fs.open(path, 'w', 0666, function(err,_fd){
+			if(err) return cb(err,0);
+			fd = _fd
+			if(buf) fdAndBuf();
+		});
+		
+		canvas.toBuffer(function(err, _buf){
+			if(err) return cb(err,0);
+			
+			buf = _buf
+			if(fd) fdAndBuf();
+		});
+
+	});
+}
---
+++ test/server.js
@@ -0,0 +1,13 @@
+var QRCode = require(__dirname+'/../qrcode')
+	, connect = require('connect');
+	
+function testQRCode(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	QRCode.toDataURL('test testing and stuff!',function(err,url){
+		if(err) console.log('error: '+err);
+		res.end("<!DOCTYPE html/><html><head><title>node-qr</title></head><body><img src='"+url+"'/></body></html>");
+	});
+}
+
+connect.createServer(testQRCode).listen(3030);
+console.log('test server started on port 3030');
