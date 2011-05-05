f51fba42265d93d3adb216e829d957a38b8d0cc3
Ryan Day <soldair@gmail.com>
2011-05-05 00:29:29 -0700

    new non poluting require strategy impmented

--- qrcode.js
+++ qrcode.js
@@ -1,20 +1,67 @@
+/*
+*copyright Ryan Day 2011
+*
+* Licensed under the MIT license:
+*   http://www.opensource.org/licenses/mit-license.php
+*
+* this is the main server side application file for node-qrcode. 
+* these exports use serverside canvas api methods for file IO and buffers
+*
+*/
+
 var QRCodeDraw = require(__dirname+'/lib/qrcode-draw')
-	, Canvas = require('canvas')
-	, fs = require('fs');
+, Canvas = require('canvas')
+, fs = require('fs')
+, drawInstance = new QRCodeDraw();
 
 //EXPORTS
-exports.QRCodeDraw = QRCodeDraw;
+/*
+so i return an instance... this is a terrible api because it kinda blocks 
+being able to do more than just extensions and the opperation of the qrcode
+lib now alters the state of the object.. 
+which is not an issue unless you need to read properties off of an object in a callback
+*/
+exports.QRCodeDraw = drawInstance;
+/*
+to make amends im adding the constriuctor to this object so extensions and instances can be made easily
+this has to be done i dont like this name but backwards compat requires that the top object be an instance
+*/
+exports.QRCodeDrawConstructor = QRCodeDraw;
+
+/*
+* provide an api to return the max characters allowed for given dimensions, and miniumum error correction level
+* the qr code library will always use the maximum error correction level for the given numbar of chars constrained by size
+*/
+exports.getMaxChars = function(minErrorCorrectionLevel,width,moduleWidth){
+	//TODO THIS NEEDS TO WORK
+};
+
 
 // returns Canvas Object with qr code drawn on it
-var draw = exports.draw = function(text,cb){
+/*
+* String text, optional Object options, Function callback
+*/
+var draw = exports.draw = function(text,options,cb){
+
+	arguments = [].slice.call(arguments);
+	cb = arguments.pop();
+	if(typeof cb != 'function') {
+		throw new TypeError('last arguement must be a function');
+	}
+	text = arguments.shift();
+	options = arguments.shift()||{};
+	
+	//TODO add optional options argument before calback
+	//TODO in order to be predictable i will provide an api that throws exceptions if the data cannot fit
+	// in the specified width. from a design perspective qr codes with dynmic data cannot break layout
 	//NOTE the width and height are determined from within the qr code lib and are not configurable from the outside yet
-	QRCodeDraw.draw(new Canvas(200,200),text,function(error,canvas){
+	drawInstance.draw(new Canvas(200,200),text,function(error,canvas){
 		cb(error,canvas)
 	});
-}
+};
 
 //returns data uri for drawn qrcode png
-var dataURL = exports.toDataURL = function(text,cb){
+exports.toDataURL = function(text,cb){
 	draw(text,function(error,canvas){
 		if(error) {
 			cb(error,'');
@@ -24,30 +71,39 @@ var dataURL = exports.toDataURL = function(text,cb){
 	});
 }
 
-//returns bytes written to file 
+//callback with bytes written to file 
 exports.save = function(path,text,cb){
 	draw(text,function(error,canvas){
 
 		var fd,buf,fdAndBuf = function(){
-			fs.write(fd, buf, 0, buf.length, 0, function(err,written){
+			fs.write(fd, buf, 0, buf.length, 0, function(error,written){
 				fs.close(fd);
-				cb(err,written);
+				if(cb) cb(error,written);
 			});
 		};
 
-		//run async calls at the same time ish so they can take advantage of the others idle time
-		canvas.toBuffer(function(err, _buf){
-			if(err) return cb(err,0);
+		//run non dependent async calls at the same time ish
+		canvas.toBuffer(function(error, _buf){
+			if(error) return cb(error,0);
 			
 			buf = _buf
 			if(fd) fdAndBuf();
 		});
 
 		fs.open(path, 'w', 0666, function(err,_fd){
-			if(err) return cb(err,0);
+			if(error) return cb(error,0);
 			fd = _fd
 			if(buf) fdAndBuf();
 		});
 
 	});
+};
+
+
+/*
+this returns an array of points that have either a 0 or 1 value representing 0 for light and 1 for dark
+these values include points in the white edge of the qrcode because that edge is actually part of the spec  
+*/
+exports.drawBits = function(text,cb){
+	//TODO THIS NEEDS TO WORK
 }
--- test/clientsideserver.js
+++ test/clientsideserver.js
@@ -1,6 +1,10 @@
 var express = require('express')
 	,app = express.createServer()
-	,fs = require('fs');
+	,fs = require('fs')
+	,QRCode = require(__dirname+'/../qrcode')
+	,canvasutil = require(__dirname+'/../../node-canvasutil/app.js')
+	,Canvas = require('canvas')
+	,Image = Canvas.Image;
 
 app.configure(function(){
 	app.use(express.methodOverride());
@@ -13,6 +17,127 @@ app.get('/', function(req, res){
 	fs.readFile(__dirname+'/clientside.html', function (err, data) {
 		res.send(data?data.toString():err);
 	});
+});
+
+app.get('/generate', function(req, res){
+	var q = req.query||{};
+	
+	QRCode.QRCodeDraw.color.light = q.lightColor||'#ffffff';
+	QRCode.QRCodeDraw.color.dark = q.darkColor||'#000000';
+	QRCode.QRCodeDraw.scale = +(q.scale);
+
+	if(isNaN(QRCode.QRCodeDraw.scale)) QRCode.QRCodeDraw.scale = 4;
+	//NOTE when i set scale too 500 canvas seg faulted
+	if(QRCode.QRCodeDraw.scale > 50) QRCode.QRCodeDraw.scale = 50;
+
+	switch(q.effect){
+		case "rounded":
+			QRCode.draw(q.text||'',function(err,canvas){
+				if(err) console.log('error: '+err);
+
+				var tpx = new canvasutil.PixelCore()
+				,luma709Only = canvasutil.conversionLib.luma709Only
+				,savedBuffer
+				,up=[],down=[],left=[],right=[]
+				,upPx,downPx,leftPx,rightPx,undefined,r,t,l,b,corner = 0;
+
+				tpx.iterate(canvas,function(px,i,len,pixels,w,h,pixelCore){
+					corner = 0;
+
+					//is dark
+					if(luma709Only(px.r,px.g,px.b) < pixelCore.threshold) {
+
+						if(i-w > 0){
+							upPx = (i-w)*4;
+							up[0] = pixels[upPx + 0];
+							up[1] = pixels[upPx + 1];
+							up[2] = pixels[upPx + 2];
+							//console.log('up',up);
+						}
+						
+						if(i+w <= len) {
+							downPx = (i+w)*4;
+							down[0] = pixels[downPx + 0];
+							down[1] = pixels[downPx + 1];
+							down[2] = pixels[downPx + 2];
+							//console.log('down',down);
+						}
+
+						//have left pixel but no wrapping
+						if(i%w != 0){
+							leftPx = (i-1)*4;
+							left[0] = pixels[leftPx + 0];
+							left[1] = pixels[leftPx + 1];
+							left[2] = pixels[leftPx + 2];
+							//console.log('left',left);
+						}
+						
+						if(i%w != w-1){
+							rightPx = (i+1)*4;
+							right[0] = pixels[rightPx + 0];
+							right[1] = pixels[rightPx + 1];
+							right[2] = pixels[rightPx + 2];
+							//console.log('right',right);
+						}
+
+						r = rightPx?luma709Only(right[0],right[1],right[2]):0;
+						t = upPx?luma709Only(up[0],up[1],up[2]):0;
+						l = leftPx?luma709Only(left[0],left[1],left[2]):0;
+						d = downPx?luma709Only(down[0],down[1],down[2]):0;
+
+						if(l > pixelCore.threshold){//if left is light and i am dark
+							if(t > pixelCore.threshold){//if top is light and i am dark
+								corner = 1;
+								pixels[rightPx + 4] = 100;
+							} else if(d > pixelCore.threshold){//if bottom is light and i am dark
+								pixels[rightPx + 4] = 100;
+								corner = 1;
+							}
+						} else if(r > pixelCore.threshold){
+							if(t > pixelCore.threshold){//if top is light and i am dark
+								corner = 1;
+							} else if(d > pixelCore.threshold){//if bottom is light and i am dark
+								corner = 1;
+							}
+						}
+						
+						if(corner) {
+							px.a = 50;
+						}
+					}
+				});
+
+				canvas.toBuffer(function(err, buf){
+					res.header('Content-Type','image/png');
+					res.send(buf);
+				});
+			});
+			break;
+		case "bacon":
+			var img = new Image();
+			img.onload = function(){
+			
+			};
+			break;
+		case "bacon-bikini":
+			
+			break;
+		default:
+			QRCode.draw(q.text||'',function(err,canvas){
+				if(err){
+					throw new Error("error drawing qr code \n"+err.message+"\n"+err.stack);
+				}
+
+				canvas.toBuffer(function(err, buf){
+					if(err){
+						throw new Error("problem getting buffer from canvas \n"+err.message+"\n"+err.stack);
+					}
+					res.header('Content-Type','image/png');
+					res.send(buf);
+				});
+			});
+			break;
+	}
 });
 
 app.listen(3031);
