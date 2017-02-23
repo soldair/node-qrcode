5f13e1e4f3dcf104fb0317bec0a7c00889831b37
Vincenzo Greco <greco.vincenzo@gmail.com>
2017-02-23 17:53:02 +0100

    Update build script

--- build.js
+++ build.js
@@ -1,67 +1,85 @@
 var spawn = require('child_process').spawn
 var fs = require('fs')
+var path = require('path')
+require('colors')
 
-var q = [
-  function () {
-    if (!fs.existsSync('./build')) {
-      fs.mkdirSync('./build')
+function createFolder (folderPath, onDone) {
+  console.log('*'.green + ' creating folder: '.grey + folderPath.white)
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath)
+  }
+
+  onDone()
+}
+
+function bundle (inputFile, exportName, outputFile, onDone) {
+  console.log('*'.green + ' bundling: '.grey +
+   inputFile.white + ' -> '.grey + outputFile.white)
+
+  var browserify = spawn('node', [
+    'node_modules/.bin/browserify',
+    inputFile,
+    '-s', exportName,
+    '-d',
+    '-o', outputFile
+  ])
+
+  browserify.stdin.end()
+  browserify.stdout.pipe(process.stdout)
+  browserify.stderr.pipe(process.stderr)
+  browserify.on('exit', function (code) {
+    if (code) {
+      console.error('browserify failed!')
+      process.exit(code)
     }
 
-    done()
-  },
+    onDone()
+  })
+}
 
-  function () {
-    var browserify = spawn('node', [
-      'node_modules/.bin/browserify',
-      'lib/index.js',
-      '-s', 'qrcodelib',
-      '-d',
-      '-o', 'build/qrcode.js'
-    ])
+function minify (inputFile, outputFile, onDone) {
+  console.log('*'.green + ' minifying: '.grey +
+   inputFile.white + ' -> '.grey + outputFile.white)
 
-    browserify.stdin.end()
-    browserify.stdout.pipe(process.stdout)
-    browserify.stderr.pipe(process.stderr)
-    browserify.on('exit', function (code) {
-      if (code) {
-        console.error('browserify failed!')
+  var uglify = spawn('node', [
+    'node_modules/.bin/uglifyjs',
+    '--compress', '--mangle',
+    '--source-map', outputFile + '.map',
+    '--source-map-url', path.basename(outputFile) + '.map',
+    '--', inputFile])
+
+  var minStream = fs.createWriteStream(outputFile)
+  uglify.stdout.pipe(minStream)
+  uglify.stdin.end()
+  uglify.on('exit', function (code) {
+    if (code) {
+      console.error('uglify failed!')
+      fs.unlink(outputFile, function () {
         process.exit(code)
-      }
-      done()
-    })
-  },
+      })
+    }
 
-  function () {
-    var uglify = spawn('node', [
-      'node_modules/.bin/uglifyjs',
-      '--compress', '--mangle',
-      '--source-map', 'build/qrcode.min.js.map',
-      '--source-map-url', 'qrcode.min.js.map',
-      '--', 'build/qrcode.js'])
+    onDone()
+  })
+}
 
-    var minStream = fs.createWriteStream('build/qrcode.min.js')
-    uglify.stdout.pipe(minStream)
-    uglify.stdin.end()
-    uglify.on('exit', function (code) {
-      if (code) {
-        console.error('uglify failed!')
-        fs.unlink('build/qrcode.min.js', function () {
-          process.exit(code)
-        })
-      }
-      done()
-    })
-  }
+var q = [
+  createFolder.bind(null, './build', done),
+  bundle.bind(null, 'lib/index.js', 'qrcodelib', 'build/qrcode.js', done),
+  bundle.bind(null, 'helper/to-sjis.js', 'qrcodelib.toSJIS', 'build/qrcode.tosjis.js', done),
+  minify.bind(null, 'build/qrcode.js', 'build/qrcode.min.js', done),
+  minify.bind(null, 'build/qrcode.tosjis.js', 'build/qrcode.tosjis.min.js', done)
 ]
 
-var done = function () {
+function done () {
   var j = q.shift()
   if (j) j()
   else complete()
 }
 
-var complete = function () {
-  console.log('build complete =)')
+function complete () {
+  console.log('\nBuild complete =)\n'.green)
 }
 
 done()
