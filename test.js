7c6ae2953bf15adb000611d9c5bcada0c85ab282
Vincenzo Greco <greco.vincenzo@gmail.com>
2017-02-23 17:54:39 +0100

    Update test script

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 var spawn = require('child_process').spawn
+var path = require('path')
 
 var opt = {
   cwd: __dirname,
   env: (function () {
-    process.env.NODE_PATH = './lib'
+    process.env.NODE_PATH = './' + path.delimiter + './lib'
     return process.env
   }()),
   stdio: [process.stdin, process.stdout, process.stderr]
@@ -12,5 +13,5 @@ var opt = {
 spawn('node', [
   'node_modules/.bin/tap',
   '--cov',
-  'test/**/*.test.js'
+  process.argv[2] || 'test/**/*.test.js'
 ], opt)
