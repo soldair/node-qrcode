673d1a3b4be5265aa960fbdd386aff449535f58e
Vincenzo Greco <greco.vincenzo@gmail.com>
2017-11-12 19:33:33 +0100

    Add width option

--- test/unit/renderer/utils.test.js
+++ test/unit/renderer/utils.test.js
@@ -3,6 +3,7 @@ var Utils = require('renderer/utils')
 
 test('Utils getOptions', function (t) {
   var defaultOptions = {
+    width: undefined,
     scale: 4,
     margin: 4,
     color: {
@@ -21,6 +22,9 @@ test('Utils getOptions', function (t) {
 
   t.equal(Utils.getOptions({ scale: 8 }).scale, 8,
     'Should return correct scale value')
+
+  t.equal(Utils.getOptions({ width: 300 }).scale, 4,
+    'Should reset scale value to default if width is set')
 
   t.equal(Utils.getOptions({ margin: null }).margin, 4,
     'Should return default margin if specified value is null')
@@ -47,6 +51,36 @@ test('Utils getOptions', function (t) {
   t.end()
 })
 
+test('Utils getScale', function (t) {
+  var symbolSize = 21
+
+  t.equal(Utils.getScale(symbolSize, { scale: 5 }), 5,
+    'Should return correct scale value')
+
+  t.equal(Utils.getScale(symbolSize, { width: 50, margin: 2 }), 2,
+    'Should calculate correct scale from width and margin')
+
+  t.equal(Utils.getScale(symbolSize, { width: 21, margin: 2, scale: 4 }), 4,
+    'Should return default scale if width is too small to contain the symbol')
+
+  t.end()
+})
+
+test('Utils getImageWidth', function (t) {
+  var symbolSize = 21
+
+  t.equal(Utils.getImageWidth(symbolSize, { scale: 5, margin: 0 }), 105,
+    'Should return correct width value')
+
+  t.equal(Utils.getImageWidth(symbolSize, { width: 250, margin: 2 }), 250,
+    'Should return specified width value')
+
+  t.equal(Utils.getImageWidth(symbolSize, { width: 10, margin: 4, scale: 4 }), 116,
+    'Should ignore width option if too small to contain the symbol')
+
+  t.end()
+})
+
 test('Utils qrToImageData', function (t) {
   t.ok(Utils.qrToImageData,
     'qrToImageData should be defined')
@@ -65,18 +99,33 @@ test('Utils qrToImageData', function (t) {
 
   var margin = 4
   var scale = 2
+  var width = 100
 
   var color = {
     dark: { r: 255, g: 255, b: 255, a: 255 },
     light: { r: 0, g: 0, b: 0, a: 255 }
   }
 
-  var imageData = []
+  var opts = {
+    margin: margin,
+    scale: scale,
+    color: color
+  }
 
+  var imageData = []
   var expectedImageSize = (sampleQrData.modules.size + margin * 2) * scale
   var expectedImageDataLength = Math.pow(expectedImageSize, 2) * 4
 
-  Utils.qrToImageData(imageData, sampleQrData, margin, scale, color)
+  Utils.qrToImageData(imageData, sampleQrData, opts)
+
+  t.equal(imageData.length, expectedImageDataLength,
+    'Should return correct imageData length')
+
+  imageData = []
+  opts.width = width
+  expectedImageDataLength = Math.pow(width, 2) * 4
+
+  Utils.qrToImageData(imageData, sampleQrData, opts)
 
   t.equal(imageData.length, expectedImageDataLength,
     'Should return correct imageData length')
