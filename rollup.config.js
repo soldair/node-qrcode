99115e530024ce2a6e44e042871cffdefb0a9fd4
Patrick <p.kuen@cloudacy.com>
2022-07-23 10:32:43 +0200

    chore: replace deprecated rollup-plugin-babel with @rollup/plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel'
+import {babel} from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
